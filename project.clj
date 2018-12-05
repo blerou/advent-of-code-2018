ad4c6917a1ca425694c4b5814861439798c80319
blerou <szabolcs.sulik@hu.ibm.com>
2018-12-05 16:02:18 +0100

    init

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject advent-of-code2018 "0.1.0-SNAPSHOT"
+  :description "solutions to advent of code 2018"
+  :url "https://adventofcode.com/2018"
+  :dependencies [[org.clojure/clojure "1.9.0"]])
