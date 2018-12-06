77245857bdb3bc1a3feaf81f3d66315f4d412e5f
blerou <szabolcs.sulik@hu.ibm.com>
2018-12-06 13:45:31 +0100

    day02 part two

--- src/advent_of_code2018/day02.clj
+++ src/advent_of_code2018/day02.clj
@@ -57,3 +57,35 @@
       [0 0])
      ; result
      (apply *))
+
+;; --- Part Two ---
+
+;; Confident that your list of box IDs is complete, you're ready to find the boxes full of prototype fabric.
+
+;; The boxes will have IDs which differ by exactly one character at the same position in both strings. For example, given the following box IDs:
+
+;; abcde
+;; fghij
+;; klmno
+;; pqrst
+;; fguij
+;; axcye
+;; wvxyz
+;; The IDs abcde and axcye are close, but they differ by two characters (the second and fourth). However, the IDs fghij and fguij differ by exactly one character, the third (h and u). Those must be the correct boxes.
+
+;; What letters are common between the two correct box IDs? (In the example above, this is found by removing the differing character from either ID, producing fgij.)
+
+(let [lines (->> (io/resource "day02-2.txt")
+                 (io/reader)
+                 (line-seq))
+      [line1 line2] (for [line1 lines
+                          line2 lines
+                          :when (->> (map = line1 line2)
+                                     (filter false?)
+                                     (count)
+                                     (= 1))]
+                      line1)]
+  (->> (map #(do [%1 %2]) line1 line2)
+       (filter #(apply = %))
+       (map first)
+       (apply str)))
