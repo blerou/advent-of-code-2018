e342df4832d6c98af15ba7e95ff2dccf3ccc200d
blerou <szabolcs.sulik@hu.ibm.com>
2018-12-12 23:08:32 +0100

    day05 part two

--- src/advent_of_code2018/day05.clj
+++ src/advent_of_code2018/day05.clj
@@ -61,3 +61,42 @@
    (->> (read-input "day05-1.txt")
         (reduce-polymer)
         (count))))
+
+;; --- Part Two ---
+
+;; Time to improve the polymer.
+
+;; One of the unit types is causing problems; it's preventing the polymer from collapsing as much as it should. Your goal is to figure out which unit type is causing the most problems, remove all instances of it (regardless of polarity), fully react the remaining polymer, and measure its length.
+
+;; For example, again using the polymer dabAcCaCBAcCcaDA from above:
+
+;;     Removing all A/a units produces dbcCCBcCcD. Fully reacting this polymer produces dbCBcD, which has length 6.
+;;     Removing all B/b units produces daAcCaCAcCcaDA. Fully reacting this polymer produces daCAcaDA, which has length 8.
+;;     Removing all C/c units produces dabAaBAaDA. Fully reacting this polymer produces daDA, which has length 4.
+;;     Removing all D/d units produces abAcCaCBAcCcaA. Fully reacting this polymer produces abCBAc, which has length 6.
+
+;; In this example, removing all C/c units was best, producing the answer 4.
+
+;; What is the length of the shortest polymer you can produce by removing all units of exactly one type and fully reacting the result?
+
+(comment
+  (into
+   #{}
+   (map #(Character/toLowerCase %))
+   [\a \b \D \c \d \B \A])
+  )
+
+(defn part-two []
+  (time
+   (let [reduced (->> (read-input "day05-2.txt")
+                      (reduce-polymer))
+         types (into #{} (map #(Character/toLowerCase %)) reduced)]
+     (reduce
+      (fn [c t]
+        (let [c2 (->> reduced
+                      (remove #{t (polarity t)})
+                      (reduce-polymer)
+                      (count))]
+          (min c c2)))
+      (count reduced)
+      types))))
